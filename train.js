7ecf6051b1c26c6459460c05b5c1a685b5a1a6c0
Zuzeth Romo <zuzethromo@yahoo.com>
2019-06-24 22:36:36 -0600

    added buttons and input functions

--- train.js
+++ train.js
@@ -11,3 +11,25 @@ let firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
   let trainInfo = firebase.database();
+
+  $("#button").on("click", function(event) {
+      event.preventDefault();
+      let name = $("#name-input").val().trim();
+      let destination = $("#destination-input").val().trim();
+      let firstTrain = $("#train-input").val().trim();
+      let frequency = $("#frequency-input").val().trim();
+
+    let newTrain = {
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    };
+
+    trainInfo.ref().push(newTrain);
+
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#train-input").val("");
+    $("#frequency-input").val("");
+  });
